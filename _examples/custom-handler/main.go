ed8b90a62133219bfd59b6b8bf993760fcc2bea4
刘备 <admin@yitianyigexiangfa.com>
2021-08-29 12:13:32 +0800

    chi example：如何自定义handler

--- _examples/custom-handler/main.go
+++ _examples/custom-handler/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+//Handler type Handler func;type Mux struct;type Compare interface;
 type Handler func(w http.ResponseWriter, r *http.Request) error
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +19,13 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// (1)chi.NewRouter()
 	r := chi.NewRouter()
+
+	// (2)Mux结构体的Mthod函数
 	r.Method("GET", "/", Handler(customHandler))
+
+	// (3)ListenAndServe函数（http包里的函数，两个参数①端口号②Mux）
 	http.ListenAndServe(":3333", r)
 }
 
